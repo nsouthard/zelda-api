6021db2224bb0052a1ca7755f7203aa5aa531cef
nsouthard <nathan.southard@gmail.com>
2017-06-08 21:32:02 -0500

    end of night commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606003150) do
+ActiveRecord::Schema.define(version: 20170609021036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "zeldas", force: :cascade do |t|
     t.string "game"
